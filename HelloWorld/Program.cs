d9266e72787238efe20889b8f07e06ba52170a50
Marcus Johnsson <mr.marcus.marega@gmail.com>
2020-03-09 11:36:09 +0100

    A modest C# start

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first ever C# app.");
+            Console.WriteLine("Its a small first step, but a step none the less...");
+            Console.ReadLine();
+        }
+    }
+}
